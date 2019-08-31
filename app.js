f9fa5c103d193360433e7ed97fd7f82d0aa99ef5
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2019-08-31 08:47:02 -0300

    Incluído tratamento de rota não encontrada e retorno de erro

--- app.js
+++ app.js
@@ -9,4 +9,17 @@ app.use(cors());
 
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  const error = new Error("Route Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: error.message
+    });
+});
+
 module.exports = app;
