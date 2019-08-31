5fa8f5ef58f3da8102eddc33c9cda99b9c45ddcf
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2019-08-31 08:45:20 -0300

    Incluído cors e a opção de incluir json.
    npm install --save cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.use(morgan('dev'));
 
