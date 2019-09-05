299842cf5c4ed7ddb31dd2201e10856b57aca675
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2019-09-05 07:54:22 -0300

    Corrigi o nome do arquivo para controller (com 2 ls)

--- routes/coffee-route.js
+++ routes/coffee-route.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const coffeeController = require('../controllers/coffee-controler');
+const coffeeController = require('../controllers/coffee-controller');
 
 module.exports = router;
