f8c770b6f1b4429971226807509fde6806eb17f5
LucasBrito <lucasbsilva29@gmail.com>
2019-09-18 12:41:16 -0300

    get em vez de post

--- routes/login-route.js
+++ routes/login-route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/login-controller');
 
-router.post("/", loginController.loginUser);
+router.get("/", loginController.loginUser);
 
 module.exports = router;
